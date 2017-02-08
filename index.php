e6e8f975131feb6a395ee67c27c2552cad2be58f
Pedro Pitillas <pedro_pitillas@dapargel.com>
2017-02-08 12:33:21 +0100

    estetic changes

--- index.php
+++ index.php
@@ -31,19 +31,19 @@ echo "
 
           <div class='masthead clearfix'>
             <div class='inner'>
-			  <h1 class='cover-heading'>Download twitter gif</h1>
-              <h2>If you like a GIF in twitter ... catch it!</h2>
-			  <img src='twitterbird.gif'>
+			  <h1 class='cover-heading'>Download twitter animated gif</h1>
+              <h2>If you like an animated GIF in twitter ... catch it!</h2>
             </div>
 			
           </div>
 
           <div class='inner cover'>	
+			<img src='twitterbird.gif'>
             <p class='lead'>
 				<form name='actua' id='actua' method='post' action='".$_SERVER['PHP_SELF']."'>
 					<label for='url' class='control-label'>Tweet url: </label>
 					<div class='input-group'>
-						<input type='url' class='form-control' name='url' id='url' value='".$url."' placeholder='Tweet url...'>
+						<input type='url' class='form-control' name='url' id='url' value='".$url."' placeholder='https://twitter.com/....'>
 					<span class='input-group-btn'>	
 						<input type='submit' class='btn btn-large  btn-primary' value='Extract' name='extraer'>
 					</span>
@@ -79,7 +79,7 @@ if($url){
 
 				echo "<div id='divresp' class='text-center'>
 				<a href='https://pbs.twimg.com/tweet_video/".$match[1].".mp4' target=_blank> Link to video (open new window)</a> 
-				<p>You can download it as .mp4 video file (left mouse & Save video) or convert into an animated .gif with a online converter</p>
+				<p>You can download it as .mp4 video file (left mouse & save video) or convert into an animated .gif with a online converter, like <a href='https://convertio.co/es/mp4-gif/' target='_blank'>Convertio</a></p>
 				</div>";
 				
 			}else{
@@ -90,19 +90,26 @@ if($url){
 				
 			}
 		}
+}else{
+
+	echo "<div class='inner-cover'>    
+				  <p class='lead'>Tweet url can be obtained in \"v\" of tweet (web version), using \" Copy link to tweet\" </p>
+				  <figure><img src='twitter-link.jpg'></figure>
+				  <p class='lead'>Twitter not serve animated GIF really ... all is MP4 format, but they say 'It's a GIF!' </p>
+				  <p class='lead'>With this tool, you can download the MP4 file and then convert it, with other service, into a GIF file...or not.</p>  
+				  
+		  </div>
+		 ";
+		 
+echo "    <div class='mastfoot'>
+            <div class='inner'>
+              <p>Get gif from a tweet , by <a href='https://simpleini.com'>Simpleteam</a>.</p>
+            </div>
+          </div> ";
 }
-
-echo "     </div>
-			  <p class='lead'><ul>
-			  <li>Tweet url can be obtained in \"v\" of tweet (web version), using \" Copy link to tweet\" <img src='twitter-link.jpg'> </li>
-			  <li>Twitter not serve animated GIF really ... all is MP4 format, but they say 'It's a GIF!' <img src='twitter-gif.png'></li>
-			  <li>With this tool, you can download the MP4 file and then convert it, with other service, into a GIF file...or not.</li>  
-			  </ul>
-			  </p>				
-
-
+echo "
+		 </div>
       </div>
-
     </div>
 	
 </body>
